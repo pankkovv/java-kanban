f9c45bdbeccf50806f90c36cda494a6f45432ef7
Панков Денис <Pankkovv@yandex.ru>
2022-10-17 14:22:12 +0300

    feat: реализация класса задачи

---
+++ src/Task.java
@@ -0,0 +1,48 @@
+public class Task {
+    protected String title;
+    protected String description;
+    protected int id;
+    protected String status;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return title + " {" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
