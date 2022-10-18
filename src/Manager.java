54427e60a480d946e4bcb3b9c7506b60c2eb013c
Панков Денис <Pankkovv@yandex.ru>
2022-10-18 21:01:21 +0300

    fix: исправить возвращаемое значение методов

--- src/Manager.java
+++ src/Manager.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 public class Manager {
-    HashMap<Integer, Task> mapTask = new HashMap<>();
+    HashMap<Integer , Task> mapTask = new HashMap<>();
     HashMap<Integer, Epic> mapEpic = new HashMap<>();
     HashMap<Integer, Subtask> mapSubtask = new HashMap<>();
 
@@ -9,32 +9,25 @@ public class Manager {
     private int idEpic = 0;
     private int idSubtask = 0;
 
-    public void getTask(String typeTask) {
-        switch (typeTask) {
-            case "task":
-                for (int id : mapTask.keySet()) {
-                    System.out.println(mapTask.get(id).toString());
-                }
-                break;
-            case "epic":
-                for (int id : mapEpic.keySet()) {
-                    System.out.println(mapEpic.get(id).toString());
-                }
-                break;
-            case "subtask":
-                for (int id : mapSubtask.keySet()) {
-                    System.out.println(mapSubtask.get(id).toString());
-                }
-                break;
-        }
+    public HashMap<Integer, Task> getTask() {
+        return mapTask;
     }
 
-    public void getListAllSubtask(int idSearch) {
+    public HashMap<Integer, Epic> getEpic() {
+        return mapEpic;
+    }
+
+    public HashMap<Integer, Subtask> getSubtask() {
+        return mapSubtask;
+    }
+
+    public HashMap<Integer, Subtask> getListAllSubtask(int idSearch) {
         if ((mapEpic.containsKey(idSearch)) && (mapEpic.get(idSearch).getListOfSubtasks().size() != 0)) {
-            System.out.println(mapEpic.get(idSearch).getListOfSubtasks().values());
+            return mapEpic.get(idSearch).getListOfSubtasks();
         } else {
             System.out.println("У данной epic-задачи отсутствуют подзадачи.");
         }
+        return null;
     }
 
     public void removeTask(String typeTask) {
@@ -43,60 +36,75 @@ public class Manager {
                 mapTask.clear();
                 break;
             case "epic":
+                for (int idSearch : mapEpic.keySet()) {
+                    Epic obj = mapEpic.get(idSearch);
+                    for (int idSubtask : obj.getListOfSubtasks().keySet())
+                        mapSubtask.remove(obj.getListOfSubtasks().get(idSearch).getId());
+                }
                 mapEpic.clear();
                 break;
             case "subtask":
+                for (int idSearch : mapSubtask.keySet()) {
+                    Subtask obj = mapSubtask.get(idSearch);
+                    mapEpic.get(obj.getIdEpic()).clearListOfSubtasks();
+                }
                 mapSubtask.clear();
                 break;
         }
         System.out.println("Все задачи удалены.");
     }
 
-    public void getTaskId(String typeTask, int idSearch) {
-        switch (typeTask) {
-            case "task":
-                if (mapTask.containsKey(idSearch)) {
-                    System.out.println(mapTask.get(idSearch).toString());
-                } else {
-                    System.out.println("Такого id задачи не существует.");
-                }
-                break;
-            case "epic":
-                if (mapEpic.containsKey(idSearch)) {
-                    System.out.println(mapEpic.get(idSearch).toString());
-                } else {
-                    System.out.println("Такого id задачи не существует.");
-                }
-                break;
-            case "subtask":
-                if (mapSubtask.containsKey(idSearch)) {
-                    System.out.println(mapSubtask.get(idSearch).toString());
-                } else {
-                    System.out.println("Такого id задачи не существует.");
-                }
-                break;
+    public Object getTaskId(int idSearch) {
+        Task obj = new Task();
+        if (mapTask.containsKey(idSearch)) {
+            obj = mapTask.get(idSearch);
+        } else {
+            System.out.println("Такого id задачи не существует.");
         }
+        return obj;
     }
 
-    public void createTask(String title, String description, String status) {
+    public Object getEpicId(int idSearch) {
+        Epic obj = new Epic();
+        if (mapEpic.containsKey(idSearch)) {
+            obj = mapEpic.get(idSearch);
+        } else {
+            System.out.println("Такого id задачи не существует.");
+        }
+        return obj;
+    }
+
+    public Object getSubtaskId(int idSearch) {
+        Subtask obj = new Subtask();
+        if (mapSubtask.containsKey(idSearch)) {
+            obj = mapSubtask.get(idSearch);
+        } else {
+            System.out.println("Такого id задачи не существует.");
+        }
+        return obj;
+    }
+
+    public Object createTask(String title, String description, String status) {
         Task obj = new Task();
         obj.setTitle(title);
         obj.setDescription(description);
         obj.setStatus(status);
         obj.setId(generatorId("task"));
         mapTask.put(obj.getId(), obj);
+        return mapTask.get(obj.getId());
     }
 
-    public void createEpic(String title, String description, String status) {
+    public Object createEpic(String title, String description, String status) {
         Epic obj = new Epic();
         obj.setTitle(title);
         obj.setDescription(description);
         obj.setStatus(status);
         obj.setId(generatorId("epic"));
         mapEpic.put(obj.getId(), obj);
+        return mapEpic.get(obj.getId());
     }
 
-    public void createSubtask(int idSearch, String title, String description, String status) {
+    public Object createSubtask(int idSearch, String title, String description, String status) {
         if (mapEpic.containsKey(idSearch)) {
             Subtask obj = new Subtask();
             obj.setTitle(title);
@@ -107,9 +115,11 @@ public class Manager {
             mapSubtask.put(obj.getId(), obj);
             mapEpic.get(idSearch).setListOfSubtasks(obj.getId(), obj);
             generatorStatusEpic(idSearch);
+            return mapSubtask.get(obj.getId());
         } else {
             System.out.println("Выбранной epic-задачи не существует.");
         }
+        return null;
     }
 
     public void updateTask(int idSearch, String title, String description, String status) {
@@ -160,7 +170,9 @@ public class Manager {
                 break;
             case "epic":
                 if (mapEpic.containsKey(idSearch)) {
-                    mapEpic.get(idSearch).getListOfSubtasks().clear();
+                    for(int idSearchSubtask : mapEpic.get(idSearch).getListOfSubtasks().keySet()){
+                        mapSubtask.remove(mapEpic.get(idSearch).getListOfSubtasks().get(idSearchSubtask).getId());
+                    }
                     mapEpic.remove(idSearch);
                 } else {
                     System.out.println("Такого id задачи не существует.");
@@ -168,7 +180,7 @@ public class Manager {
                 break;
             case "subtask":
                 if (mapSubtask.containsKey(idSearch)) {
-                    mapEpic.get(mapSubtask.get(idSearch).getIdEpic()).getListOfSubtasks().remove(idSearch);
+                    mapEpic.get(mapSubtask.get(idSearch).getIdEpic()).removeListOfSubtasks(idSearch);
                     mapSubtask.remove(idSearch);
                 } else {
                     System.out.println("Такого id задачи не существует.");
@@ -198,16 +210,16 @@ public class Manager {
     }
 
     public void generatorStatusEpic(int idSearch) {
-        boolean conditionOne = mapEpic.get(idSearch).listOfSubtasks.size() == 0;
+        boolean conditionOne = mapEpic.get(idSearch).getListOfSubtasks().size() == 0;
         boolean conditionTwo = false;
         boolean conditionThree = false;
 
-        for (int idSubtask : mapEpic.get(idSearch).listOfSubtasks.keySet()) {
-            conditionTwo = mapEpic.get(idSearch).listOfSubtasks.get(idSubtask).status.equals("NEW");
+        for (int idSubtask : mapEpic.get(idSearch).getListOfSubtasks().keySet()) {
+            conditionTwo = mapEpic.get(idSearch).getListOfSubtasks().get(idSubtask).status.equals("NEW");
         }
 
-        for (int idSubtask : mapEpic.get(idSearch).listOfSubtasks.keySet()) {
-            conditionThree = mapEpic.get(idSearch).listOfSubtasks.get(idSubtask).status.equals("DONE");
+        for (int idSubtask : mapEpic.get(idSearch).getListOfSubtasks().keySet()) {
+            conditionThree = mapEpic.get(idSearch).getListOfSubtasks().get(idSubtask).status.equals("DONE");
         }
 
         if (conditionOne || conditionTwo) {
