52f64d45f4f35c892014d4eb4fdef9103017816f
Панков Денис <Pankkovv@yandex.ru>
2022-10-18 21:01:48 +0300

    fix: исправить вызов сеттера

--- src/Epic.java
+++ src/Epic.java
@@ -1,14 +1,22 @@
 import java.util.HashMap;
 
 public class Epic extends Task {
-    HashMap<Integer, Subtask> listOfSubtasks = new HashMap<>();
+    private HashMap<Integer, Subtask> listOfSubtasks = new HashMap<>();
 
     public HashMap<Integer, Subtask> getListOfSubtasks() {
         return listOfSubtasks;
     }
 
-    public void setListOfSubtasks(Integer idSearch, Subtask subtask) {
-        this.listOfSubtasks.put(idSearch, subtask);
+    public void setListOfSubtasks(int idSearch, Subtask obj) {
+        listOfSubtasks.put(idSearch, obj);
+    }
+
+    public void clearListOfSubtasks() {
+        listOfSubtasks.clear();
+    }
+
+    public void removeListOfSubtasks(int idSearch){
+        listOfSubtasks.remove(idSearch);
     }
 
     @Override
