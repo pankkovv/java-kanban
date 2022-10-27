b9e3f0613ad759c93914f79c3249dbe0fe8859d4
Панков Денис <Pankkovv@yandex.ru>
2022-10-27 15:18:55 +0300

    feat: переопределить методы интерфейса менеджера задач

--- src/InMemoryTaskManager.java
+++ src/InMemoryTaskManager.java
@@ -1,27 +1,39 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Manager {
+public class InMemoryTaskManager implements TaskManager {
+
+    HistoryManager managerHistory = Managers.getDefaultHistory();
     List<Task> listTask = new ArrayList<>();
     List<Epic> listEpic = new ArrayList<>();
     List<Subtask> listSubtask = new ArrayList<>();
+
+    enum StatusOfTask {
+        NEW,
+        IN_PROGRESS,
+        DONE
+    }
 
     private int idTask = 0;
     private int idEpic = 0;
     private int idSubtask = 0;
 
+    @Override
     public List<Task> getTask() {
         return listTask;
     }
 
+    @Override
     public List<Epic> getEpic() {
         return listEpic;
     }
 
+    @Override
     public List<Subtask> getSubtask() {
         return listSubtask;
     }
 
+    @Override
     public List<Subtask> getListAllSubtask(int idSearch) {
         for (Epic epic : listEpic) {
             if (epic.id == idSearch) {
@@ -33,60 +45,70 @@ public class Manager {
         return null;
     }
 
-    public void removeTask(String typeTask) {
-        switch (typeTask) {
-            case "task":
-                listTask.clear();
-                break;
-            case "epic":
-                for (Epic epic : listEpic) {
-                    for (Subtask subtask : epic.getListOfSubtasks()) {
-                        listSubtask.remove(subtask);
-                    }
-                }
-                listEpic.clear();
-                break;
-            case "subtask":
-                for (Subtask subtask : listSubtask) {
-                    for (Epic epic : listEpic) {
-                        if (epic.getListOfSubtasks().contains(subtask)) {
-                            epic.removeListOfSubtasks(subtask);
-                        }
-                    }
-                }
-                listSubtask.clear();
-                break;
-        }
+    @Override
+    public void removeTask() {
+        listTask.clear();
         System.out.println("Все задачи удалены.");
     }
 
+    @Override
+    public void removeEpic() {
+        for (Epic epic : listEpic) {
+            for (Subtask subtask : epic.getListOfSubtasks()) {
+                listSubtask.remove(subtask);
+            }
+        }
+        listEpic.clear();
+        System.out.println("Все epic-задачи удалены.");
+    }
+
+    @Override
+    public void removeSubtask() {
+        for (Subtask subtask : listSubtask) {
+            for (Epic epic : listEpic) {
+                if (epic.getListOfSubtasks().contains(subtask)) {
+                    epic.removeListOfSubtasks(subtask);
+                }
+            }
+        }
+        listSubtask.clear();
+        System.out.println("Все subtask-задачи удалены.");
+    }
+
+    @Override
     public Task getTaskId(int idSearch) {
         for (Task task : listTask) {
             if (task.getId() == idSearch) {
+                managerHistory.add(task);
                 return task;
             }
         }
         return null;
     }
 
+    @Override
     public Epic getEpicId(int idSearch) {
         for (Epic epic : listEpic) {
             if (epic.getId() == idSearch) {
+                managerHistory.add(epic);
                 return epic;
             }
         }
         return null;
     }
 
+    @Override
     public Subtask getSubtaskId(int idSearch) {
         for (Subtask subtask : listSubtask) {
             if (subtask.getId() == idSearch) {
+                managerHistory.add(subtask);
                 return subtask;
             }
         }
         return null;
     }
 
+    @Override
     public Task createTask(String title, String description, String status) {
         Task task = new Task();
         task.setTitle(title);
@@ -97,6 +119,7 @@ public class Manager {
         return task;
     }
 
+    @Override
     public Epic createEpic(String title, String description, String status) {
         Epic epic = new Epic();
         epic.setTitle(title);
@@ -107,6 +130,7 @@ public class Manager {
         return epic;
     }
 
+    @Override
     public Subtask createSubtask(int idSearch, String title, String description, String status) {
         for (Epic epic : listEpic) {
             if (epic.getId() == idSearch) {
@@ -125,6 +149,7 @@ public class Manager {
         return null;
     }
 
+    @Override
     public void updateTask(int idSearch, String title, String description, String status) {
         for (Task task : listTask) {
             if (task.getId() == idSearch) {
@@ -135,6 +160,7 @@ public class Manager {
         }
     }
 
+    @Override
     public void updateEpic(int idSearch, String title, String description) {
         for (Epic epic : listEpic) {
             if (epic.getId() == idSearch) {
@@ -145,6 +171,7 @@ public class Manager {
         }
     }
 
+    @Override
     public void updateSubtask(int idSearchEpic, int idSearch, String title, String description, String status) {
         for (Epic epic : listEpic) {
             if (epic.getId() == idSearchEpic) {
@@ -161,43 +188,45 @@ public class Manager {
         }
     }
 
-    public void removeTaskId(String typeTask, int idSearch) {
-        switch (typeTask) {
-            case "task":
-                for (Task task : listTask) {
-                    if (task.getId() == idSearch) {
-                        listTask.remove(task);
-                        return;
-                    }
-                }
-                break;
-            case "epic":
-                for (Epic epic : listEpic) {
-                    if (epic.getId() == idSearch) {
-                        if (epic.getListOfSubtasks().size() != 0) {
-                            for (Subtask subtask : epic.getListOfSubtasks()) {
-                                listSubtask.remove(subtask);
-                            }
-                        }
-                        listEpic.remove(epic);
-                        return;
-                    }
-                }
-                break;
-            case "subtask":
-                for (Subtask subtask : listSubtask) {
-                    if (subtask.getId() == idSearch) {
-                        for (Epic epic : listEpic) {
-                            epic.removeListOfSubtasks(subtask);
-                        }
-                        listSubtask.remove(subtask);
-                        return;
-                    }
-                }
-                break;
+    @Override
+    public void removeTaskId(int idSearch) {
+        for (Task task : listTask) {
+            if (task.getId() == idSearch) {
+                listTask.remove(task);
+                return;
+            }
         }
     }
 
+    @Override
+    public void removeEpicId(int idSearch) {
+        for (Epic epic : listEpic) {
+            if (epic.getId() == idSearch) {
+                if (epic.getListOfSubtasks().size() != 0) {
+                    for (Subtask subtask : epic.getListOfSubtasks()) {
+                        listSubtask.remove(subtask);
+                    }
+                }
+                listEpic.remove(epic);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void removeSubtaskId(int idSearch) {
+        for (Subtask subtask : listSubtask) {
+            if (subtask.getId() == idSearch) {
+                for (Epic epic : listEpic) {
+                    epic.removeListOfSubtasks(subtask);
+                }
+                listSubtask.remove(subtask);
+                return;
+            }
+        }
+    }
+
+    @Override
     public int generatorId(String typeTask) {
         int id = 0;
 
@@ -218,6 +247,7 @@ public class Manager {
         return id;
     }
 
+    @Override
     public void generatorStatusEpic(int idSearch) {
         boolean conditionOne = false;
         boolean conditionTwo = false;
@@ -227,24 +257,30 @@ public class Manager {
             if (epic.getId() == idSearch) {
                 conditionOne = epic.getListOfSubtasks().size() == 0;
                 for (Subtask subtask : epic.getListOfSubtasks()) {
-                    conditionTwo = subtask.getStatus().equals("NEW");
-                    conditionThree = subtask.getStatus().equals("DONE");
+                    conditionTwo = subtask.getStatus().equals(String.valueOf(StatusOfTask.NEW));
+                    conditionThree = subtask.getStatus().equals(String.valueOf(StatusOfTask.DONE));
                 }
             }
 
             if (conditionOne || conditionTwo) {
                 if (epic.getId() == idSearch) {
-                    epic.setStatus("NEW");
+                    epic.setStatus(String.valueOf(StatusOfTask.NEW));
                 }
             } else if (conditionThree) {
                 if (epic.getId() == idSearch) {
-                    epic.setStatus("DONE");
+                    epic.setStatus(String.valueOf(StatusOfTask.DONE));
                 }
             } else {
                 if (epic.getId() == idSearch) {
-                    epic.setStatus("IN_PROGRESS");
+                    epic.setStatus(String.valueOf(StatusOfTask.IN_PROGRESS));
                 }
             }
         }
     }
+
+    @Override
+    public List<Task> getHistory(){
+        return managerHistory.getHistory();
+    }
+
 }
