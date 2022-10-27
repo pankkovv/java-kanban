ccc46ec28d965c830aba4085c75363de46e3a6d3
Панков Денис <Pankkovv@yandex.ru>
2022-10-27 15:15:17 +0300

    feat: создать интерфейс для менеджера задач

---
+++ src/TaskManager.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+public interface TaskManager {
+
+    List<Task> getTask();
+
+    List<Epic> getEpic();
+
+    List<Subtask> getSubtask();
+
+    List<Subtask> getListAllSubtask(int idSearch);
+
+    void removeTask();
+
+    void removeEpic();
+
+    void removeSubtask();
+
+    Task getTaskId(int idSearch);
+
+    Epic getEpicId(int idSearch);
+
+    Subtask getSubtaskId(int idSearch);
+
+    Task createTask(String title, String description, String status);
+
+    Epic createEpic(String title, String description, String status);
+
+    Subtask createSubtask(int idSearch, String title, String description, String status);
+
+    void updateTask(int idSearch, String title, String description, String status);
+
+    void updateEpic(int idSearch, String title, String description);
+
+    void updateSubtask(int idSearchEpic, int idSearch, String title, String description, String status);
+
+    void removeTaskId(int idSearch);
+
+    void removeEpicId(int idSearch);
+
+    void removeSubtaskId(int idSearch);
+
+    int generatorId(String typeTask);
+
+    void generatorStatusEpic(int idSearch);
+
+    List<Task> getHistory();
+}
