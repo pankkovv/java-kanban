127ddb2c773555fd903986e8fcc5ef11039a8b08
Панков Денис <Pankkovv@yandex.ru>
2022-10-27 15:18:33 +0300

    feat: переопределить методы интерфейса истории

---
+++ src/InMemoryHistoryManager.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    List<Task> listHistory = new ArrayList<>();
+
+    @Override
+    public void add(Task task){
+        listHistory.add(task);
+    }
+
+    @Override
+    public List<Task> getHistory(){
+        if(listHistory.size() != 0) {
+            for (int i = 0; i < listHistory.size(); i++){
+                if (listHistory.size() < 10) {
+                    return listHistory;
+                } else {
+                    listHistory.remove(0);
+                }
+            }
+        } else {
+            System.out.println("Вы не просматривали определенные задачи. История пуста.");
+        }
+        return null;
+    }
+}
