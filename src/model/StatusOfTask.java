fec9f0f5d545c6baab304588ec2f20d8c29a3885
Панков Денис <Pankkovv@yandex.ru>
2022-10-29 13:29:59 +0300

    refactor: сгруппировать файлы в пакет model

--- src/model/StatusOfTask.java
+++ src/model/StatusOfTask.java
@@ -1,3 +1,5 @@
+package model;
+
 public enum StatusOfTask {
     NEW,
     IN_PROGRESS,
--- src/model/Task.java
+++ src/model/Task.java
@@ -1,3 +1,5 @@
+package model;
+
 public class Task {
     protected String title;
     protected String description;
